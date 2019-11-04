17be727e3f60d33124e471e4f7844ebf517b5936
Bertil Andersen <bertilandersen@hotmail.com>
2019-11-04 13:56:12 +0100

    Add project files.

---
+++ week7ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Write a program that defines a shape class with a constructor that gives value to width and height. 
+Then define two sub-classes, triangle and rectangle, that calculate the area of the shape area. 
+Test the two sub-classes by defining two variables, a triangle and a rectangle, and then call the area function for these two variables. 
+  */
+namespace week7ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
